f5affeee4662fcb5a33599d782b70be60fa50adb
Ray Nguyen(NGUYỄN VĂN TRÁNG) <Ray.Nguyen1@quantaqmh.com>
2025-05-15 08:47:15 +0700

    attribute bindings

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup>
+import HomeComponent from './components/HomeComponent.vue';
+
 </script>
 
 <template>
-  <h1>6h learn vue</h1>
+  <!-- <h1>6h learn vue</h1> -->
+  <HomeComponent/>
 </template>
 
 <style scoped>
---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+const myChannel = "https://www.youtube.com/watch?v=FN7ALfpGxiI"
+const myImage = 'https://i.ytimg.com/vi/FN7ALfpGxiI/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLCBitBHhvsVYOot-1_N6mJyCrthTw'
+const myAlt = "Image information"
+const imageHeight = 200
+const imageWidth = 300
+
+</script>
+<template>
+    <h1>Home component</h1>    
+    <a v-bind:href="myChannel">My Channel</a>
+    <img :src="myImage" :alt="myAlt" :height="imageHeight" :width="imageWidth" />
+</template>
+
+<style scoped>
+
+</style>
